7d1ea0d80d11220281700704a0dc06d0d828a8fc
NoobLk <125950957+nooblk-98@users.noreply.github.com>
2025-10-01 21:26:14 +0530

    update teerform configs

--- terraform-infra/providers/aws/main.tf
+++ terraform-infra/providers/aws/main.tf
@@ -13,3 +13,71 @@ resource "aws_instance" "free_ec2" {
     Name = "FreeTierEC2"
   }
 }
+
+# Uncomment resources below as needed:
+
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = { Name = "main-vpc" }
+# }
+
+# resource "aws_subnet" "main" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "us-east-1a"
+#   tags = { Name = "main-subnet" }
+# }
+
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   tags = { Name = "allow_ssh" }
+# }
+
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 20
+#   engine               = "mysql"
+#   engine_version       = "8.0"
+#   instance_class       = "db.t3.micro"
+#   name                 = "mydb"
+#   username             = "foo"
+#   password             = "barbazqux"
+#   parameter_group_name = "default.mysql8.0"
+#   skip_final_snapshot  = true
+# }
+
+# resource "aws_iam_user" "example" {
+#   name = "example-user"
+# }
+
+# resource "aws_lambda_function" "example" {
+#   function_name = "example_lambda"
+#   handler       = "index.handler"
+#   runtime       = "nodejs14.x"
+#   role          = aws_iam_role.lambda_exec.arn
+#   filename      = "lambda.zip"
+# }
+
+# resource "aws_elb" "example" {
+#   name               = "example-elb"
+#   availability_zones = ["us-east-1a"]
+#   listener {
+#     instance_port     = 80
+#     instance_protocol = "http"
+#     lb_port           = 80
+#     lb_protocol       = "http"
+#   }
+# }
--- terraform-infra/providers/azure/main.tf
+++ terraform-infra/providers/azure/main.tf
@@ -6,3 +6,67 @@ resource "azurerm_resource_group" "sample" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Uncomment resources below as needed:
+
+# resource "azurerm_virtual_network" "main" {
+#   name                = "main-vnet"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.sample.name
+# }
+
+# resource "azurerm_subnet" "main" {
+#   name                 = "main-subnet"
+#   resource_group_name  = azurerm_resource_group.sample.name
+#   virtual_network_name = azurerm_virtual_network.main.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
+
+# resource "azurerm_network_interface" "main" {
+#   name                = "main-nic"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.sample.name
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.main.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
+
+# resource "azurerm_linux_virtual_machine" "main" {
+#   name                = "main-vm"
+#   resource_group_name = azurerm_resource_group.sample.name
+#   location            = var.location
+#   size                = "Standard_B1s"
+#   admin_username      = "azureuser"
+#   network_interface_ids = [azurerm_network_interface.main.id]
+#   admin_password      = "Password1234!"
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
+
+# resource "azurerm_storage_account" "main" {
+#   name                     = "examplestorageacct"
+#   resource_group_name      = azurerm_resource_group.sample.name
+#   location                 = var.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# resource "azurerm_sql_server" "main" {
+#   name                         = "mysqlserver"
+#   resource_group_name          = azurerm_resource_group.sample.name
+#   location                     = var.location
+#   version                      = "12.0"
+#   administrator_login          = "sqladmin"
+#   administrator_login_password = "Password1234!"
+# }
